9909afafa9bfe63be29c8aa9d8963a237fcc851d
wesleyarizio <wesleyarizio26@gmail.com>
2021-06-19 17:08:07 -0300

    finish news list

--- frontend/src/components/News/NewsModal/index.js
+++ frontend/src/components/News/NewsModal/index.js
@@ -22,7 +22,14 @@ const NewsModal = ({ newsId, close }) => {
                     <FaTimes size={20} color={`#373f41`} onClick={() => close()} />
                 </header>
                 <br />
-                    <p>By {data.newsData.user.name} at {data.newsData.news.created_at}</p>
+                    <section className="author-section">
+                        <p>Posted By <span className="author-name">{data.newsData.user.name}</span></p>
+                        <p>at&nbsp;
+                            {
+                               new Intl.DateTimeFormat('pt-BR').format(new Date(data.newsData.news.created_at)) 
+                            }
+                        </p>
+                    </section>
                 <br />
                 <p>{data.newsData.news.body}</p>
             </div>
